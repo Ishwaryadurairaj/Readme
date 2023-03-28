1f0e6759496b7e39ebe2cf52b5c49df6fa8d623b
Ishwarya durairaj <ishwaryadurairaj1989@gmail.com>
2023-03-28 18:16:46 +0530

    add some conditons

--- Opengoogle.java
+++ Opengoogle.java
@@ -4,8 +4,13 @@ public class Opengoogle {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver_win32\\chromedriver_99.exe");
+
+		
 		WebDriver driver = new ChromeDriver();
+
 		driver.get("http://www.google.com");
+		System.out.println("welcome");
+		driver.quit();
 	}
 
 }
