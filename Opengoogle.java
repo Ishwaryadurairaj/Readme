db15b1f77661556a4cd129b6e4839adff0ce5825
Ishwarya durairaj <ishwaryadurairaj1989@gmail.com>
2023-03-28 15:34:36 +0530

    Tc2803 - opengoogle in java

---
+++ Opengoogle.java
@@ -0,0 +1,11 @@
+
+public class Opengoogle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver_win32\\chromedriver_99.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+	}
+
+}
